c40180410750092f9770fd75c5d23b06f1557c1d
LL-yibei <yibei.liu@thoughtworks.com>
2024-02-23 21:46:46 +0800

    fix: fix config file

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'LL-yibei', // Usually your GitHub org/user name.
   projectName: 'LL-yibei.github.io', // Usually your repo name.
-  deploymentBranch: 'main',
+  deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
